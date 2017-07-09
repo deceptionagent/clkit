5c4ec0c2060297a4515e8511351549b1ce9027f3
chelsea <chelsea@thermodynamics.local>
2017-07-08 20:03:55 -0700

    add CLKAssert tests

---
+++ Unit Tests/Test_CLKAssert.m
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) 2017 Plastic Pulse. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "CLKAssert.h"
+
+
+@interface Test_CLKAssert : XCTestCase
+
+@end
+
+
+@implementation Test_CLKAssert
+
+- (void)testCLKHardAssert
+{
+    XCTAssertNoThrow(CLKHardAssert((1 == 1), @"FooError", @"lorem %@", @"ipsum"));
+    XCTAssertThrowsSpecificNamed((CLKHardAssert(1 == 2, @"FooError", @"lorem %@", @"ipsum")), NSException, @"FooError");
+    XCTAssertThrowsSpecificNamed((CLKHardAssert((1 == 2), @"FooError", @"lorem %@", @"ipsum")), NSException, @"FooError");
+    XCTAssertThrowsSpecificNamed((CLKHardAssert((1 == 2), @"FooError", @"lorem")), NSException, @"FooError");
+}
+
+- (void)testCLKHardParameterAssert
+{
+    XCTAssertNoThrow(CLKHardParameterAssert((1 == 1)));
+    XCTAssertThrowsSpecificNamed(CLKHardParameterAssert(1 == 2), NSException, NSInvalidArgumentException);
+    XCTAssertThrowsSpecificNamed(CLKHardParameterAssert((1 == 2)), NSException, NSInvalidArgumentException);
+}
+
+@end
