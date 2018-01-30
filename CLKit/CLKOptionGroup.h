43a526f8123885e77c95257460127a89c08b3cd9
mikey <mikey@thermodynamics.local>
2018-01-29 21:31:04 -0800

    optionless groups:
    groups are now weakly associated with options, referring to them by identifier (name) than by option instances. this makes it clear the split between option registration (the options array of the parser) and group references. not only is it now impossible to mistakenly attempt to register options via groups, weird option mismatches involving groups are now eliminated.

--- CLKit/CLKOptionGroup.h
+++ CLKit/CLKOptionGroup.h
@@ -12,14 +12,14 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CLKOptionGroup : NSObject
 
-+ (instancetype)groupWithOptions:(NSArray<CLKOption *> *)options required:(BOOL)required;
-+ (instancetype)mutexedGroupWithOptions:(NSArray<CLKOption *> *)options required:(BOOL)required;
-+ (instancetype)mutexedGroupWithOptions:(nullable NSArray<CLKOption *> *)options subgroups:(nullable NSArray<CLKOptionGroup *> *)subgroups required:(BOOL)required;
++ (instancetype)groupForOptionsNamed:(NSArray<NSString *> *)options required:(BOOL)required;
++ (instancetype)mutexedGroupForOptionsNamed:(NSArray<NSString *> *)options required:(BOOL)required;
++ (instancetype)mutexedGroupForOptionsNamed:(nullable NSArray<NSString *> *)options subgroups:(nullable NSArray<CLKOptionGroup *> *)subgroups required:(BOOL)required;
 
 + (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
 
-@property (nullable, readonly) NSArray<CLKOption *> *options;
+@property (nullable, readonly) NSArray<NSString *> *options;
 @property (nullable, readonly) NSArray<CLKOptionGroup *> *subgroups;
 @property (readonly) BOOL mutexed;
 @property (readonly) BOOL required; // at least one member of this group is required
