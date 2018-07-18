bd9390eb57bc21a221cb857fc22650511b4ff431
chelsea <chelsea@thermodynamics.local>
2018-07-17 23:54:10 -0700

    add a class to transport information from a verb to a verb depot's caller. can also be used to transport parser/validator failures from inside a depot to the depot's caller.

---
+++ CLKit/CLKCommandResult.h
@@ -0,0 +1,23 @@
+//
+//  Copyright (c) 2018 Plastic Pulse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CLKCommandResult : NSObject
+
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)resultWithExitStatus:(int)exitStatus;
++ (instancetype)resultWithExitStatus:(int)exitStatus errors:(nullable NSArray<NSError *> *)errors;
+
+- (instancetype)initWithExitStatus:(int)exitStatus errors:(nullable NSArray<NSError *> *)errors userInfo:(nullable NSDictionary *)userInfo NS_DESIGNATED_INITIALIZER;
+
+@property (readonly) int exitStatus;
+@property (nullable, readonly) NSArray<NSError *> *errors;
+@property (nullable, readonly) NSString *errorDescription;
+@property (nullable, readonly) NSDictionary *userInfo;
+
+@end
