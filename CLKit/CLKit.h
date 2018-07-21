b6542da27bbb15439827705936150ed1fc204708
chelsea <chelsea@thermodynamics.local>
2018-07-21 03:30:09 -0700

    update clklab to verbs 2.0

---
+++ CLKit/CLKit.h
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) 2018 Plastic Pulse. All rights reserved.
+//
+
+#import "CLKArgumentManifest.h"
+#import "CLKArgumentParser.h"
+#import "CLKArgumentTransformer.h"
+#import "CLKCommandResult.h"
+#import "CLKError.h"
+#import "CLKOption.h"
+#import "CLKOptionGroup.h"
+#import "CLKVerb.h"
+#import "CLKVerbDepot.h"
+#import "NSArray+CLKAdditions.h"
