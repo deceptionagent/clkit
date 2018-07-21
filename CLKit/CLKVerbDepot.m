7cb4d8e04c067156ac12f97ee0d8b1c3ad919310
chelsea <chelsea@thermodynamics.local>
2018-07-20 22:00:21 -0700

    initial verb depot work

--- CLKit/CLKVerbDepot.m
+++ CLKit/CLKVerbDepot.m
@@ -4,8 +4,26 @@
 
 #import "CLKVerbDepot.h"
 
+#import <sysexits.h>
+
+#import "CLKArgumentParser.h"
 #import "CLKAssert.h"
+#import "CLKCommandResult.h"
+#import "CLKError.h"
 #import "CLKVerb.h"
+#import "NSMutableArray+CLKAdditions.h"
+#import "NSError+CLKAdditions.h"
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLKVerbDepot ()
+
+- (CLKCommandResult *)_runVerb:(id<CLKVerb>)verb withArgumentVector:(NSArray<NSString *> *)argumentVector;
+
+@end
+
+NS_ASSUME_NONNULL_END
 
 
 @implementation CLKVerbDepot
@@ -16,7 +34,7 @@
 
 - (instancetype)initWithArgumentVector:(NSArray<NSString *> *)argumentVector verbs:(NSArray<id<CLKVerb>> *)verbs
 {
-    CLKHardParameterAssert(argumentVector.count > 0);
+    CLKHardParameterAssert(argumentVector != nil);
     CLKHardParameterAssert(verbs.count > 0);
     
     self = [super init];
@@ -25,7 +43,7 @@
         
         _verbMap = [[NSMutableDictionary alloc] init];
         for (id<CLKVerb> verb in verbs) {
-            CLKHardAssert((_verbMap[verb.name] == nil), NSInvalidArgumentException, @"duplicate verb name: '%@'", verb.name);
+            CLKHardAssert((_verbMap[verb.name] == nil), NSInvalidArgumentException, @"encountered multiple verbs named '%@'", verb.name);
             _verbMap[verb.name] = verb;
         }
     }
@@ -37,6 +55,37 @@
 {
     [_argumentVector release];
     [super dealloc];
+}
+
+#pragma mark -
+
+- (CLKCommandResult *)dispatchVerb
+{
+    if (_argumentVector.count == 0) {
+        NSError *error = [NSError clk_CLKErrorWithCode:CLKErrorNoVerbSpecified description:@"No verb specified."];
+        return [CLKCommandResult resultWithExitStatus:EX_USAGE errors:@[ error ]];
+    }
+    
+    NSMutableArray<NSString *> *remainingArguments = [[_argumentVector mutableCopy] autorelease];
+    NSString *verbName = [remainingArguments clk_popFirstObject];
+    id<CLKVerb> verb = _verbMap[verbName];
+    if (verb == nil) {
+        NSError *error = [NSError clk_CLKErrorWithCode:CLKErrorUnrecognizedVerb description:@"%@: Unrecognized verb.", verbName];
+        return [CLKCommandResult resultWithExitStatus:EX_USAGE errors:@[ error ]];
+    }
+    
+    return [self _runVerb:verb withArgumentVector:remainingArguments];
+}
+
+- (CLKCommandResult *)_runVerb:(id<CLKVerb>)verb withArgumentVector:(NSArray<NSString *> *)argumentVector
+{
+    CLKArgumentParser *parser = [CLKArgumentParser parserWithArgumentVector:argumentVector options:verb.options optionGroups:verb.optionGroups];
+    CLKArgumentManifest *manifest = [parser parseArguments];
+    if (manifest == nil) {
+        return [CLKCommandResult resultWithExitStatus:EX_USAGE errors:parser.errors];
+    }
+    
+    return [verb runWithManifest:manifest];
 }
 
 @end
--- Unit Tests/Test_CLKVerbDepot.m
+++ Unit Tests/Test_CLKVerbDepot.m
@@ -4,21 +4,42 @@
 
 #import <XCTest/XCTest.h>
 
+#import <sysexits.h>
+
+#import "CLKCommandResult.h"
 #import "CLKVerb.h"
 #import "CLKVerbDepot.h"
 #import "StuntVerb.h"
+#import "NSError+CLKAdditions.h"
 
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface Test_CLKVerbDepot : XCTestCase
 
+- (void)_performDispatchTestWithDepot:(CLKVerbDepot *)depot expectedResult:(CLKCommandResult *)expectedResult;
+
 @end
 
+NS_ASSUME_NONNULL_END
+
+
 @implementation Test_CLKVerbDepot
+
+- (void)_performDispatchTestWithDepot:(CLKVerbDepot *)depot expectedResult:(CLKCommandResult *)expectedResult
+{
+    CLKCommandResult *result = [depot dispatchVerb];
+    XCTAssertNotNil(result);
+    XCTAssertEqual(result.exitStatus, expectedResult.exitStatus);
+    XCTAssertEqualObjects(result.errors, expectedResult.errors);
+}
+
+#pragma mark -
 
 - (void)testInit
 {
     NSArray *argv = @[ @"flarn", @"--barf" ];
-    NSArray<id<CLKVerb>> *verbs = @[
+    NSArray *verbs = @[
         [StuntVerb flarnVerb],
         [StuntVerb quoneVerb]
     ];
@@ -26,10 +47,12 @@
     CLKVerbDepot *depot = [[[CLKVerbDepot alloc] initWithArgumentVector:argv verbs:verbs] autorelease];
     XCTAssertNotNil(depot);
     
+    depot = [[[CLKVerbDepot alloc] initWithArgumentVector:@[] verbs:verbs] autorelease];
+    XCTAssertNotNil(depot);
+    
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wnonnull"
     XCTAssertThrows([[[CLKVerbDepot alloc] initWithArgumentVector:nil verbs:verbs] autorelease]);
-    XCTAssertThrows([[[CLKVerbDepot alloc] initWithArgumentVector:@[] verbs:verbs] autorelease]);
     XCTAssertThrows([[[CLKVerbDepot alloc] initWithArgumentVector:argv verbs:nil] autorelease]);
     XCTAssertThrows([[[CLKVerbDepot alloc] initWithArgumentVector:argv verbs:@[]] autorelease]);
 #pragma clang diagnostic pop
@@ -38,7 +61,7 @@
 - (void)testInit_collisionGuard
 {
     NSArray *argv = @[ @"flarn", @"--barf" ];
-    NSArray<id<CLKVerb>> *verbs = @[
+    NSArray *verbs = @[
         [StuntVerb flarnVerb],
         [StuntVerb quoneVerb],
         [StuntVerb flarnVerb]
@@ -54,6 +77,34 @@
 //    ];
 //
 //    XCTAssertThrows([[[CLKVerbDepot alloc] initWithArgumentVector:argv verbs:verbs] autorelease]);
+}
+
+- (void)test_dispatchVerb_emptyArgumentVector
+{
+    NSArray *verbs = @[ [StuntVerb flarnVerb] ];
+    NSError *expectedError = [NSError clk_CLKErrorWithCode:CLKErrorNoVerbSpecified description:@"No verb specified."];
+    CLKCommandResult *expectedResult = [CLKCommandResult resultWithExitStatus:EX_USAGE errors:@[ expectedError ]];
+    
+    CLKVerbDepot *depot = [[[CLKVerbDepot alloc] initWithArgumentVector:@[] verbs:verbs] autorelease];
+    [self _performDispatchTestWithDepot:depot expectedResult:expectedResult];
+}
+
+- (void)test_dispatchVerb_unrecognizedVerb
+{
+    NSArray *verbs = @[ [StuntVerb flarnVerb] ];
+    NSError *expectedError = [NSError clk_CLKErrorWithCode:CLKErrorUnrecognizedVerb description:@"barf: Unrecognized verb."];
+    CLKCommandResult *expectedResult = [CLKCommandResult resultWithExitStatus:EX_USAGE errors:@[ expectedError ]];
+    
+    CLKVerbDepot *depot = [[[CLKVerbDepot alloc] initWithArgumentVector:@[ @"barf" ] verbs:verbs] autorelease];
+    [self _performDispatchTestWithDepot:depot expectedResult:expectedResult];
+    
+    depot = [[[CLKVerbDepot alloc] initWithArgumentVector:@[ @"barf", @"--quone" ] verbs:verbs] autorelease];
+    [self _performDispatchTestWithDepot:depot expectedResult:expectedResult];
+
+    expectedError = [NSError clk_CLKErrorWithCode:CLKErrorUnrecognizedVerb description:@"--quone: Unrecognized verb."];
+    expectedResult = [CLKCommandResult resultWithExitStatus:EX_USAGE errors:@[ expectedError ]];
+    depot = [[[CLKVerbDepot alloc] initWithArgumentVector:@[ @"--quone", @"barf" ] verbs:verbs] autorelease];
+    [self _performDispatchTestWithDepot:depot expectedResult:expectedResult];
 }
 
 @end
