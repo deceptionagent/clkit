c9e43a192d2a8767587e9bdf7b96cc9e42b47c66
mikey <mikey@thermodynamics.local>
2018-07-24 02:15:59 -0700

    add family support to clklab, add CLKVerbFamily to public umbrella header

---
+++ clklab/DeliveryVerb.m
@@ -0,0 +1,39 @@
+//
+//  Copyright (c) 2018 Plastic Pulse. All rights reserved.
+//
+
+#import "DeliveryVerb.h"
+
+
+@implementation DeliveryVerb
+
+- (NSString *)name
+{
+    return @"delivery";
+}
+
+- (NSArray<CLKOption *> *)options
+{
+    return @[
+        [CLKOption optionWithName:@"ex" flag:@"e"],
+        [CLKOption optionWithName:@"cathedra" flag:@"c"]
+    ];
+}
+
+- (NSArray<CLKOptionGroup *> *)optionGroups
+{
+    return @[
+        [CLKOptionGroup mutexedGroupForOptionsNamed:@[ @"ex", @"cathedra" ] required:YES]
+    ];
+}
+
+- (CLKCommandResult *)runWithManifest:(CLKArgumentManifest *)manifest
+{
+    fprintf(stdout, "=================================================\n");
+    fprintf(stdout, "# %s\n", self.name.UTF8String);
+    fprintf(stdout, "=================================================\n");
+    fprintf(stdout, "%s\n", manifest.debugDescription.UTF8String);
+    return [CLKCommandResult resultWithExitStatus:0];
+}
+
+@end
