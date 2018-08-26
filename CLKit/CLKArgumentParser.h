375e44740a1a2fcee343f8199b9438e2e3adda50
chelsea <chelsea@thermodynamics.local>
2018-08-26 02:26:38 -0700

    rename ArgumentParserSpec -> ArgumentParserResultSpec

--- CLKit/CLKArgumentParser.h
+++ CLKit/CLKArgumentParser.h
@@ -13,11 +13,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CLKArgumentParser : NSObject
 
-+ (instancetype)parserWithArgumentVector:(NSArray<NSString *> *)argv options:(NSArray<CLKOption *> *)options;
-+ (instancetype)parserWithArgumentVector:(NSArray<NSString *> *)argv options:(NSArray<CLKOption *> *)options optionGroups:(nullable NSArray<CLKOptionGroup *> *)groups;
-
 + (instancetype)new NS_UNAVAILABLE;
 - (instancetype)init NS_UNAVAILABLE;
+
++ (instancetype)parserWithArgumentVector:(NSArray<NSString *> *)argv options:(NSArray<CLKOption *> *)options;
++ (instancetype)parserWithArgumentVector:(NSArray<NSString *> *)argv options:(NSArray<CLKOption *> *)options optionGroups:(nullable NSArray<CLKOptionGroup *> *)groups;
 
 - (nullable CLKArgumentManifest *)parseArguments;
 
