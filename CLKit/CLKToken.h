95905491fd8510e052a107a4112acb80a13abfb7
chelsea <chelsea@thermodynamics.local>
2019-01-21 22:04:40 -0800

    WIP: refactor token sniffing

---
+++ CLKit/CLKToken.h
@@ -0,0 +1,13 @@
+//
+//  CLKToken.h
+//  CLKit
+//
+//  Created by mikey on 21/1/19.
+//  Copyright © 2019 Plastic Pulse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLKToken : NSObject
+
+@end
---
+++ CLKit/CLKToken.m
@@ -0,0 +1,13 @@
+//
+//  CLKToken.m
+//  CLKit
+//
+//  Created by mikey on 21/1/19.
+//  Copyright © 2019 Plastic Pulse. All rights reserved.
+//
+
+#import "CLKToken.h"
+
+@implementation CLKToken
+
+@end
