585ef995ce119954b4f7df011ffac60069b5f228
mikey <mikey@thermodynamics.local>
2019-02-10 20:16:00 -0800

    remove some unused test case additions that are messing with coverage numbers

--- Unit Tests/XCTestCase+CLKAdditions.h
+++ Unit Tests/XCTestCase+CLKAdditions.h
@@ -20,10 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface XCTestCase (CLKAdditions)
 
-- (void)verifyError:(NSError *)error domain:(NSString *)domain code:(NSInteger)code;
 - (void)verifyError:(NSError *)error domain:(NSString *)domain code:(NSInteger)code description:(NSString *)description;
-- (void)verifyError:(NSError *)error domain:(NSString *)domain code:(NSInteger)code userInfo:(NSDictionary *)userInfo;
-- (void)verifyCLKError:(NSError *)error code:(CLKError)code description:(NSString *)description;
 
 - (CLKArgumentManifest *)manifestWithSwitchOptions:(nullable NSDictionary<CLKOption *, NSNumber *> *)switchOptions parameterOptions:(nullable NSDictionary<CLKOption *, NSArray *> *)parameterOptions;
 - (CLKArgumentManifestValidator *)validatorWithSwitchOptions:(nullable NSDictionary<CLKOption *, NSNumber *> *)switchOptions parameterOptions:(nullable NSDictionary<CLKOption *, id> *)parameterOptions;
